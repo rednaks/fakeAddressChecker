351b42d1e46bc2fc50a05e0bd9afd5164297e320
Alexandre BM <s@rednaks.tn>
2013-11-09 12:09:15 +0100

    Preparing the interface

---
+++ content/overlay.js
@@ -0,0 +1,11 @@
+Components.utils.import("resource:///modules/gloda/index_msg.js");
+Components.utils.import("resource:///modules/gloda/mimemsg.js");
+// This is Thunderbird 3.3 only!
+Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://gre/modules/NetUtil.jsm");
+
+var FakeAddress = {
+  check: function () {
+
+ }
+}
